115b69efd78a6ab6c3d221d7f95e66f85b1a677f
Olga Ovcharova <ovcharova.on@gmail.com>
2020-01-22 21:14:55 +0300

    добавен цикл

--- js/stat.js
+++ js/stat.js
@@ -35,19 +35,31 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
+// var blueColor = function getColor(){
+//     return "hsl(" + 250 * Math.random() + ',' +
+//                (25 + 70 * Math.random()) + '%,' +
+//                (85 + 10 * Math.random()) + '%)'
+// };
+
 window.renderStatistics = function(ctx, names, times) {
   renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  // ctx.rotate(1.5 * Math.PI);
 
   renderText(ctx);
   ctx.fillStyle = '#000';
 
+
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    if (names[i] === "Вы"){
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'blue';
+    };
     ctx.fillRect(CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y + GAP + TEXT_WIDTH, BAR_WIDTH, (barHeight * times[i]) / maxTime);
-    ctx.fillStyle = 'hsl(240, 100%,' + Math.random() * '50%';
     ctx.strokeText(names[i], CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_Y + GAP + barHeight + GAP);
   }
+
 };
