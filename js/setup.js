0322adf4323692010ad2f461ef2f0c2d4d4f5210
Olga Ovcharova <ovcharova.on@gmail.com>
2020-01-28 20:33:05 +0300

    Исправления замечаний

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,7 @@
 
 var wizardNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var wizardSurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColor = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_NUMBER = 4;
 
@@ -36,11 +36,11 @@ function createWizard() {
 }
 
 function createWizards() {
-  var wizards = [];
+  var wizardsList = [];
   for (var i = 0; i < WIZARDS_NUMBER; ++i) {
-    wizards.push(createWizard());
+    wizardsList.push(createWizard());
   }
-  return wizards;
+  return wizardsList;
 }
 
 var wizards = createWizards();
